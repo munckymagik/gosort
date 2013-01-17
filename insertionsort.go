44bca31af9f07019ac438c9a998584e7e1262d36
Dan Munckton <dan@munckton.co.uk>
2013-01-17 21:54:48 +0000

    Sorting:  add explanatory comments to the existing algorithms

--- insertionsort.go
+++ insertionsort.go
@@ -4,16 +4,20 @@ package main
  * Sort an array of integers, in place, using the insertion sort algorithm.
  */
 func InsertionSort(a []int) {
+    // Iterate up from the second element to the last
 	for j := 1; j < len(a); j++ {
 		key := a[j]
 		i := j - 1
 
+        // Iterate down through the sorted subset [j-1 ... 0], shifting
+        // elements upwards until key's position is found
 		for i >= 0 && a[i] > key {
-			a[i+1] = a[i]
-			i--
+			a[i + 1] = a[i]
+            i--
 		}
 
-		a[i+1] = key
+        // Insert key into its position in the sorted subset
+		a[i + 1] = key
 	}
 }
 
