5a45cedac80771b1c9f50483a372f6c13c7c678b
Dan Munckton <munckfish@gmail.com>
2013-04-28 12:27:18 +0100

    Add a file to home the package documentation

---
+++ packagedoc.go
@@ -0,0 +1,8 @@
+/*
+Package gosort contains a set of sorting algorithm implementations.
+
+The source code been created for educational use, consequently only sorting of
+integer arrays is supported and no effort has been made to optimise beyond the
+efficiency of the algorithms themselves.
+*/
+package gosort
