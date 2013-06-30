c84bc956647a5b72280d6094e6eed7bde65b6fd8
Dan Munckton <daniel@feynman.(none)>
2013-06-30 15:40:04 +0100

    Add first version of heap sort using a min heap

---
+++ minheap.go
@@ -0,0 +1,100 @@
+package gosort
+
+type MinHeap struct {
+    heap     []int
+    heapSize int
+}
+
+func NewMinHeap(storage []int) *MinHeap {
+    newHeap := new(MinHeap)
+    newHeap.heap = storage
+    newHeap.buildMinHeap()
+
+    return newHeap
+}
+
+func HeapSort(input []int) {
+    heap := NewMinHeap(input)
+    heap.inplaceSort()
+}
+
+func (self *MinHeap) Insert(newItem int) {
+}
+
+func (self *MinHeap) Min() int {
+    return self.heap[0]
+}
+
+func (self *MinHeap) ExtractMin() int {
+    return 0
+}
+
+func (self *MinHeap) buildMinHeap() {
+    self.heapSize = len(self.heap)
+    for i := self.heapSize / 2; i >= 0; i-- {
+        self.minHeapify(i)
+    }
+}
+
+func (self *MinHeap) minHeapify(i int) {
+    l := left(i)
+    r := right(i)
+    smallest := i
+
+    if l < self.heapSize && self.heap[l] < self.heap[i] {
+        smallest = l
+    }
+    if r < self.heapSize && self.heap[r] < self.heap[smallest] {
+        smallest = r
+    }
+    if i != smallest {
+        self.heap[i], self.heap[smallest] = self.heap[smallest], self.heap[i]
+        self.minHeapify(smallest)
+    }
+}
+
+func (self *MinHeap) checkInvariant() bool {
+    for i := 1; i < self.heapSize; i++ {
+
+        // The current node should be greater than or equal to the parent
+        invariant := self.heap[i] >= self.heap[parent(i)]
+
+        if !invariant {
+            return false
+        }
+    }
+
+    return true
+}
+
+func (self *MinHeap) inplaceSort() {
+    heapSize := self.heapSize
+
+    for i := self.heapSize - 1; i > 0; i-- {
+        self.heap[0], self.heap[i] = self.heap[i], self.heap[0]
+        self.heapSize--
+        self.minHeapify(0)
+    }
+
+    // The array comes out reverse sorted, so we need to reverse it
+    // TODO is there a way to avoid this?
+    start := 0
+    end := heapSize - 1
+    for start < end {
+        self.heap[start], self.heap[end] = self.heap[end], self.heap[start]
+        start++
+        end--
+    }
+}
+
+func parent(i int) int {
+    return (i - 1) / 2
+}
+
+func left(i int) int {
+    return 2*i + 1
+}
+
+func right(i int) int {
+    return 2*i + 2
+}
--- sorting_test.go
+++ sorting_test.go
@@ -75,3 +75,21 @@ func TestQuickSortPivotChoosers(t *testing.T) {
         runTests(curriedQuicksort, t)
     }
 }
+
+func TestHeapSort(t *testing.T) {
+    runTests(HeapSort, t)
+}
+
+func TestHeapSortInvariant(t *testing.T) {
+    for _, fixture := range fixtures {
+        // clone the fixture to prevent accidental modification
+        var cloned_fixture []int = make([]int, len(fixture))
+        copy(cloned_fixture, fixture)
+
+        heap := NewMinHeap(cloned_fixture)
+
+        if !heap.checkInvariant() {
+            t.Error("Fails invariant")
+        }
+    }
+}
