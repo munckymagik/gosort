327762caa646177596f82f9ae8ab80ef8402209d
Dan Munckton <daniel@feynman.(none)>
2013-06-30 19:54:56 +0100

    Implement reading, sorting and output in gosort executable

--- gosort/gosort.go
+++ gosort/gosort.go
@@ -3,6 +3,7 @@ package main
 import (
     // Standard imports
     "fmt"
+    "io"
     "os"
     "strings"
     "text/template"
@@ -11,26 +12,26 @@ import (
     "github.com/munckymagik/gosort"
 )
 
-type algorithm struct {
+type sortAlgorithm struct {
     // The name that the user can specify on the command line
     // to select this algorithm
     Name string
 
     // The algorithm to apply to the input
-    Algorithm gosort.IntegerSorter
+    Run gosort.IntegerSorter
 
     // The usage description
     Description string
 }
 
-var algorithmsList = []algorithm{
+var algorithmsList = []sortAlgorithm{
     {"quicksort", gosort.QuickSort, "Sort using the Quick Sort algorithm"},
     {"mergesort", gosort.MergeSort, "Sort using the Merge Sort algorithm"},
     {"insertionsort", gosort.InsertionSort, "Sort using the Insetion Sort algorithm"},
     {"bubblesort", gosort.BubbleSort, "Sort using the Bubble Sort algorithm"},
 }
 
-var algorithmsMap = make(map[string]algorithm)
+var algorithmsMap = make(map[string]sortAlgorithm)
 
 var usageTemplate = `gosort is a test front-end for common sorting algorithms.
 
@@ -72,9 +73,28 @@ func main() {
     }
 
     if theAlgorithm, ok := algorithmsMap[requestedAlgorithm]; ok {
-        fmt.Println("You chose", theAlgorithm.Name)
+        fmt.Fprintln(os.Stderr, "Chosen algorithm", theAlgorithm.Name)
 
-        // TODO implement the reading stdin, sorting and outputing
+        buffer := make([]int, 0)
+        var itemBuffer int
+
+        itemsRead, err := fmt.Scanln(&itemBuffer)
+        for itemsRead == 1 && err == nil {
+            buffer = append(buffer, itemBuffer)
+            itemsRead, err = fmt.Scanln(&itemBuffer)
+        }
+
+        if err != io.EOF {
+            fmt.Println("ERROR reading input:", err)
+            os.Exit(1)
+        }
+
+        theAlgorithm.Run(buffer)
+
+        for _, element := range buffer {
+            fmt.Println(element)
+        }
+
     } else {
         fmt.Println("ERROR Unrecognised algorithm/command:", requestedAlgorithm)
         fmt.Println("Run gosort help to see the list of supported choices")
