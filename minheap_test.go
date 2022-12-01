0587e37745886ec8c5a68da958358a84acb29559
Dan Munckton <munckfish@gmail.com>
2022-12-01 22:21:10 +0000

    Simplify and DRY test fixture handling

--- minheap_test.go
+++ minheap_test.go
@@ -3,12 +3,8 @@ package gosort
 import "testing"
 
 func TestHeapSortInvariant(t *testing.T) {
-	for _, fixture := range fixtures {
-		// clone the fixture to prevent accidental modification
-		var cloned_fixture []int = make([]int, len(fixture))
-		copy(cloned_fixture, fixture)
-
-		heap := NewMinHeap(cloned_fixture)
+	for _, fixture := range fixtures() {
+		heap := NewMinHeap(fixture)
 
 		if !heap.checkInvariant() {
 			t.Error("Fails invariant")
--- sorting_test.go
+++ sorting_test.go
@@ -26,39 +26,37 @@ func TestSortingAlgos(t *testing.T) {
 	}
 }
 
-var fixtures = map[string][]int{
-	"Should Handle Nil":            nil,
-	"Should Handle Empty":          {},
-	"Should Handle One Element":    {0},
-	"Should Handle Already Sorted": {1, 2, 3, 4, 5},
-	"Should Sort Reversed":         {5, 4, 3, 2, 1},
-	"Should Sort Shuffled":         {4, 5, 3, 1, 2},
-	"Should Sort Duplicates":       {6, 6, 4, 3, 8, 8, 3, 0, 3, 8},
+func fixtures() map[string][]int {
+	return map[string][]int{
+		"Should Handle Nil":            nil,
+		"Should Handle Empty":          {},
+		"Should Handle One Element":    {0},
+		"Should Handle Already Sorted": {1, 2, 3, 4, 5},
+		"Should Sort Reversed":         {5, 4, 3, 2, 1},
+		"Should Sort Shuffled":         {4, 5, 3, 1, 2},
+		"Should Sort Duplicates":       {6, 6, 4, 3, 8, 8, 3, 0, 3, 8},
+	}
 }
 
 func testSorting(alg func([]int), t *testing.T) {
-	for desc, fixture := range fixtures {
-		// clone the fixture to prevent accidental modification
-		var cloned_fixture []int = make([]int, len(fixture))
-		copy(cloned_fixture, fixture)
-
+	for desc, fixture := range fixtures() {
 		// Execute in a separate function in order to handle errors
 		func() {
 			// Handle errors
 			defer func() {
 				if err := recover(); err != nil {
 					t.Errorf("Error occured in '%s'. Err: %s, in: %+v, out: %+v",
-						err, desc, fixture, cloned_fixture)
+						err, desc, fixture, fixture)
 				}
 			}()
 
 			// Now execute the test
-			alg(cloned_fixture)
+			alg(fixture)
 
 			// Check that the operation was successful
-			if !sort.IntsAreSorted(cloned_fixture) {
+			if !sort.IntsAreSorted(fixture) {
 				t.Errorf("Test '%s' failed. In: %v, out: %v",
-					desc, fixture, cloned_fixture)
+					desc, fixture, fixture)
 			}
 		}()
 	}
