554573e58e83875e1d77a049e14634fbed88ad14
Dan Munckton <munckfish@gmail.com>
2022-12-01 22:23:56 +0000

    Nuke type deprecation

--- types.go
+++ types.go
@@ -5,8 +5,3 @@ import "golang.org/x/exp/constraints"
 // Sorter defines the signature for a function that sorts a slice of
 // ordered elements in-place.
 type Sorter[T constraints.Ordered] func(a []T)
-
-// IntegerSorter defines the signature for a function that sorts a slice of
-// integers in-place.
-// Deprecated: use Sorter[T] instead.
-type IntegerSorter Sorter[int]
